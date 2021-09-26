2974a103a998456aef253c5a6e710932afc59f9b
moro <ricardoromero4444@gmail.com>
2021-09-26 14:06:13 -0300

    ✨ : Basic svelte-web3 setup

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,6 +4,18 @@
 
 <script lang="ts">
 	import Board from '$lib/components/board/Board.svelte';
+	import { onMount } from 'svelte';
+	import {
+		defaultChainStore,
+		web3,
+	} from 'svelte-web3';
+
+	// Connect to metamask
+	onMount(() => defaultChainStore.setBrowserProvider());
+
+	let balance
+	$:  if ($web3) { balance = $web3.eth.getBalance('0x76fc18e74dd215467a5ed990ceb322c94df01de9') }
+
 </script>
 
 <svelte:head>
